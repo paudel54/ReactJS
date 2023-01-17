e09e077df2926b37b0aa00345398a7b2c807fb14
paudel54 <2sansrit@gmail.com>
2023-01-17 15:15:18 +0545

    Rendered 1st plain text in React, making out with basic layout

--- jsx/src/index.js
+++ jsx/src/index.js
@@ -1,17 +1,24 @@
+//first gets executed and lets create step by step procedures.
+
+//1)Import the React and ReactDom Libraries to show content on screen
+
+//react is a base library, that can be handy to use for different apps like mobile or web
+//it is used for creating components, browsers dont understand react so there came need of reactDOM
+//reactDOM, renders all the components that's written into a web. 
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
-);
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+
+//2)Get a reference to the html div file name index.js with ID root
+const el = document.getElementById('root');
+//3)Tell React to take control of that element, we want to show element inside this
+const root = ReactDOM.createRoot(el);
+//4) Create a component
+//component is a function that returns some JSX  
+function App() {
+  return <h1> Hello Mr, Kim</h1>
+}
+
+//5) Show the component on the screen 
+root.render(<App />);
