fa8b25112123b4f94a2c6cee464779acd34dbd20
paudel54 <2sansrit@gmail.com>
2023-01-18 07:26:19 +0545

    Created basic React component

---
+++ jsx/src/App.js
@@ -0,0 +1,6 @@
+
+function App() {
+    return <h1>Hi There!</h1>
+}
+
+export default App;
--- jsx/src/index.js
+++ jsx/src/index.js
@@ -7,46 +7,16 @@
 //reactDOM, renders all the components that's written into a web. 
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-
+//Import a component from other file
+import App from './App';
 
 
 //2)Get a reference to the html div file name index.js with ID root
 const el = document.getElementById('root');
 //3)Tell React to take control of that element, we want to show element inside this
 const root = ReactDOM.createRoot(el);
-//4) Create a component
-//component is a function that returns some JSX  
-function App() {
-  //return is always req to render in screen , if its not returned it's not shown in the screen.
-  //reducing the use of two diff variable to single argument
-  // const date = new Date();
-  // const time = date.toLocaleTimeString();
-  // return <h1> {new Date().toLocaleTimeString()} </h1>
-  // const name = 'Donald Trump' 
-  // return <div> My name is:
-  //   <h1>{name}</h1>
-  // </div>
-  // return <input type="number" style={{ border: '3px solid blue' }} min={5} />
 
-  //jsx prop follows camelCase Property
-  // return <textarea autoFocus={true} />
 
-  return (
-    //class attrubute must be camelcase here className is camel case
-    <div className="wrapper">
-      <textarea
-        readOnly
-        //num prop should be in curly braket
-        maxLength={3}
-        //boolena true written just the property name its auto reconized as true
-        spellCheck
-        //inline style provided a object use of curly brakets of 2 set, outer set 
-        style={{ backgroundColor: 'gray' }}
-      />
-    </div>
-  )
-
-}
 
 //5) Show the component on the screen 
 root.render(<App />);
