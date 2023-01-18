390c2ade5d5eda7bb1b3c37fdbfbbfa1467a18d4
paudel54 <2sansrit@gmail.com>
2023-01-18 14:27:31 +0545

    2nd mini project to understand react state is initaited

---
+++ animals/src/AnimalShow.js
@@ -0,0 +1,7 @@
+//component 1 
+
+function AnimalShow() {
+    return <div>Cow!</div>
+}
+
+export default AnimalShow;
---
+++ animals/src/App.js
@@ -0,0 +1,8 @@
+//App.js
+function App() {
+    return <div>
+        Show animal List Here!
+    </div>
+}
+
+export default App;
