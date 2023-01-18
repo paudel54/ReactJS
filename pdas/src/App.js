8a57582684f16eed840dfdc79f9bf89c1ed49a72
paudel54 <2sansrit@gmail.com>
2023-01-18 10:46:44 +0545

    included image

--- pdas/src/App.js
+++ pdas/src/App.js
@@ -1,9 +1,17 @@
+//importing js file we do nt' need extension
 import ProfileCard from "./ProfileCard";
+//adding image with import use
+import AlexaImage from './images/alexa.png';
+import CortanaImage from './images/cortana.png';
+import SiriImage from './images/siri.png';
 
 function App() {
     return (
         <div>
             <div>Personal Digital Assistance</div>
+            {/* displayed image */}
+            <img src={AlexaImage} />
+            <img src={CortanaImage} />
             {/* //here the profile card will make styles or prop which then passes as object into child component, here ProfileCard Component
             //the passed data is grabbed synchronously by child component ProfileCard and renders and returns one at time. */}
             <ProfileCard title="Alexa" handle="@alexa" />
