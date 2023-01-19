cc0671dfea4220d1e650d33bf009cfef5dae7e6b
paudel54 <2sansrit@gmail.com>
2023-01-19 11:46:40 +0545

    Added events system onto image

--- animals/src/AnimalShow.js
+++ animals/src/AnimalShow.js
@@ -1,3 +1,4 @@
+import { useState } from 'react';
 //component 1 
 import bird from './svg/bird.svg';
 import cat from './svg/cat.svg';
@@ -19,11 +20,24 @@ const svgMap = {
 
 //we can receive whole obj as prop or obj destructing what we want tht can be written
 function AnimalShow({ type }) {
+    //use of State System
+    const [clicks, setClicks] = useState(0);
+
+    //created handler function
+    const handleClick = () => {
+        setClicks(clicks + 1);
+
+    };
     // console.log({ type })
     // {
     //     "type": "cat"
     //   }
-    return <div> <img alt="animal" src={svgMap[type]} /> </div>
+    return (
+        <div onClick={handleClick}>
+            <img alt="animal" src={svgMap[type]} />
+            <img alt="heart" src={heart} style={{ width: 10 + 10 * clicks + 'px' }} />
+        </div>
+    );
 }
 
 export default AnimalShow;
