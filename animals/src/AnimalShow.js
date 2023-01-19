84552ca6972d1ccc4d187a38f4758fc6ad60f2e8
paudel54 <2sansrit@gmail.com>
2023-01-19 11:30:38 +0545

    Added images with dynamic src pointing to key of an array

--- animals/src/AnimalShow.js
+++ animals/src/AnimalShow.js
@@ -1,11 +1,29 @@
 //component 1 
+import bird from './svg/bird.svg';
+import cat from './svg/cat.svg';
+import cow from './svg/cow.svg';
+import dog from './svg/dog.svg';
+import gator from './svg/gator.svg';
+import heart from './svg/heart.svg';
+import horse from './svg/horse.svg';
+
+//creating key for animals with svgMap obj
+const svgMap = {
+    bird,
+    cat,
+    cow,
+    dog,
+    gator,
+    horse
+};
+
 //we can receive whole obj as prop or obj destructing what we want tht can be written
 function AnimalShow({ type }) {
     // console.log({ type })
     // {
     //     "type": "cat"
     //   }
-    return <div>{type}</div>
+    return <div> <img alt="animal" src={svgMap[type]} /> </div>
 }
 
 export default AnimalShow;
