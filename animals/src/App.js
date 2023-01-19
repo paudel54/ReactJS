5ef05d8cc9a3ae8df9220f43f56b22ed68ca90dd
paudel54 <2sansrit@gmail.com>
2023-01-19 09:40:51 +0545

    Picking a Random element in react

--- animals/src/App.js
+++ animals/src/App.js
@@ -1,27 +1,30 @@
 //imported useState module to rerender screen on change in state system;
 import { useState } from 'react';
 
+function getRandomAnimal() {
+    const animals = ['bird', 'cat', 'cow', 'dog', 'gator', 'horse'];
+    return animals[Math.floor(Math.random() * animals.length)]
+}
+
+// console.log(getRandomAnimal())
+
 function App() {
-    //Array Destructing 
-    //we dont change count , like count = 5;
-    ///1 defined useState to change state
-    const [count, setCount] = useState(0);
-    //Arrow function auto returns when called!
-    //event handlesers
-    const handleMouseMove = () => {
-        //update the state by calling setter func
-        setCount(count + 1);
+
+    //initiated with empty array
+    const [animals, setAnimals] = useState([]);
+
+    const handleClick = () => {
+        //calling function getRandomAnimal and stores onto the ...animals array
+        setAnimals([...animals, getRandomAnimal()]);
     };
 
-    return <section>
-        {/* adding events named handleClick */}
-        {/* <button onClick={handleClick}> Add Animal</button> */}
-        {/* //uuse the state */}
-        {/* <div>Number of Animals: {count} </div> */}
-        <div onMouseMove={handleMouseMove} style={{ height: '200px', width: '200px', backgroundColor: 'lightblue' }}>Move Mouse Here</div>
-        {/* added event handler*/}
-        <div>Number of onMouseMove event:{count} </div>
-    </section>
+    return (
+        <div>
+            <button onClick={handleClick}>Add Animal</button>
+            <div>{animals}</div>
+        </div>
+
+    );
 }
 
 export default App;
