1b93bb0c811c04f0f6dbb6d820c3b15ca2578d75
paudel54 <2sansrit@gmail.com>
2023-01-20 17:51:08 +0545

    handling input Elements with 5 simple steps ignoring styles like of vanilla js docs.query

--- 3_pics/src/components/SearchBar.js
+++ 3_pics/src/components/SearchBar.js
@@ -1,6 +1,9 @@
 //Here onSubmit prop is taken out from App.js custom component prop name
-function SearchBar({ onSubmit }) {
+//1.defining  new state for handling input elements
+import { useState } from 'react';
 
+function SearchBar({ onSubmit }) {
+    const [term, setTerm] = useState('');
     onSubmit('carss')
 
     const handleFormSubmit = (event) => {
@@ -8,10 +11,18 @@ function SearchBar({ onSubmit }) {
         console.log('Passing data from child to parent ')
     };
 
+    //2.Event handler to watch for the 'onChange' event
+    const handleChange = (event) => {
+        //3.Got value from input with event.target.value
+        // console.log(event.target.value);
+        //4.use with setter func to take value from input and update
+        setTerm(event.target.value);
+    };
+
     return <div>
         {/* wrapping the input element inside form element  porvides a power to submit the form auto when user hits enter*/}
         <form onSubmit={handleFormSubmit}>
-            <input />
+            <input value={term} onChange={handleChange} />
         </form>
 
         {/* <button onClick={handleClick}>Click here</button> */}
