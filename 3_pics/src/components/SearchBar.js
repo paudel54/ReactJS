903da70bbd8e93cd76fd7aacf93a011c60f53473
paudel54 <2sansrit@gmail.com>
2023-01-20 17:07:04 +0545

    handling form submission

--- 3_pics/src/components/SearchBar.js
+++ 3_pics/src/components/SearchBar.js
@@ -1,13 +1,20 @@
 //Here onSubmit prop is taken out from App.js custom component prop name
 function SearchBar({ onSubmit }) {
-    console.log(onSubmit);
-    const handleClick = () => {
-        onSubmit('Cars');
-    }
+
+    onSubmit('carss')
+
+    const handleFormSubmit = (event) => {
+        event.preventDefault();
+        console.log('Passing data from child to parent ')
+    };
 
     return <div>
-        <input />
-        <button onClick={handleClick}>Click here</button>
+        {/* wrapping the input element inside form element  porvides a power to submit the form auto when user hits enter*/}
+        <form onSubmit={handleFormSubmit}>
+            <input />
+        </form>
+
+        {/* <button onClick={handleClick}>Click here</button> */}
     </div>
 }
 
