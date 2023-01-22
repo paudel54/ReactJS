fc239b238ff6bab93475a09eb29a8efd8ef0e6c9
paudel54 <2sansrit@gmail.com>
2023-01-22 09:05:56 +0545

    adding async to func &, await var to event handlers

--- 3_pics/src/App.js
+++ 3_pics/src/App.js
@@ -1,20 +1,25 @@
 //
 import SearchBar from './components/SearchBar'
 //App be the parent , SearchBar be a child component
+
+import SearchImages from './api'
 function App() {
     // Creating event handler for handleSubmit
-    const handleSubmit = (term) => {
-        console.log('Do a search with', term);
+    const handleSubmit = async (term) => {
+        // console.log('Do a search with', term);
+        const result = await SearchImages(term);
+        console.log(result);
     };
 
-    return (<div>
+    return (
+        <div>
 
-        {/* Passing props to SearchBar Component, for cutom component we call prop with any name */}
-        {/* we can call onSubmit with any custom prop Name */}
-        {/* only in case of use of builtin custom btn we need to use definate name */}
-        {/* only thing that matters is name of prop matches to child comp */}
-        <SearchBar onSubmit={handleSubmit} />
-    </div>);
+            {/* Passing props to SearchBar Component, for cutom component we call prop with any name */}
+            {/* we can call onSubmit with any custom prop Name */}
+            {/* only in case of use of builtin custom btn we need to use definate name */}
+            {/* only thing that matters is name of prop matches to child comp */}
+            <SearchBar onSubmit={handleSubmit} />
+        </div>);
 }
 
 export default App;
--- 3_pics/src/components/SearchBar.js
+++ 3_pics/src/components/SearchBar.js
@@ -4,19 +4,25 @@ import { useState } from 'react';
 
 function SearchBar({ onSubmit }) {
     const [term, setTerm] = useState('');
-    onSubmit('carss')
+    // onSubmit('carss')
 
     const handleFormSubmit = (event) => {
         event.preventDefault();
-        console.log('Passing data from child to parent ')
+        console.log('Passing data from child to parent ');
+        onSubmit(term);
     };
 
-    //2.Event handler to watch for the 'onChange' event
+    //2.Event handler to watch for the 'onChange' . whenever this handler is called is called
+    //we receive event object
     const handleChange = (event) => {
         //3.Got value from input with event.target.value
         // console.log(event.target.value);
         //4.use with setter func to take value from input and update
-        setTerm(event.target.value.replace([/a-z/], ''));
+
+        // we can only type upper case letters or number by use of regex
+        //setTerm(event.target.value.replace([/a-z/], ''));
+        setTerm(event.target.value);
+
     };
 
     return <div>
@@ -25,6 +31,7 @@ function SearchBar({ onSubmit }) {
             {/* term is a var from useState */}
             Confirm your Search: {term}
             <br></br>
+            {/* wiring up the event handler to input telling up with current value must be term or '' state */}
             <input value={term} onChange={handleChange} />
         </form>
 
