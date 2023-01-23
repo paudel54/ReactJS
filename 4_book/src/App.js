a7fb5a0a80c81eb660444c8d6f688b149cd26016
paudel54 <2sansrit@gmail.com>
2023-01-23 17:16:10 +0545

    added updates to book with creating new array list with array destructing

--- 4_book/src/App.js
+++ 4_book/src/App.js
@@ -6,6 +6,19 @@ function App() {
     //event handler to pass as prop
     //for custom event handler we can pass prop name of our choice
     const createBook = (title) => {
+        //Bad Code practise for updating array on state change
+        // books.push({id: 14, title: title});
+        console.log(books);
+        // setBooks(books);
+
+        //updating book list
+        const updatedBooks = [
+            ...books,
+            { id: 123, title: title }
+        ];
+
+        setBooks(updatedBooks);
+
         console.log('Need to add book with', title)
     };
     return <div>
