fc2782b99ea5df89bbc1a0bff59056c2e76d929e
paudel54 <2sansrit@gmail.com>
2023-01-23 12:39:13 +0545

    communicate app and components, for receiving new titles

--- 4_book/src/App.js
+++ 4_book/src/App.js
@@ -1,5 +1,16 @@
+import { useState } from 'react';
+import BookCreate from './components/BookCreate';
+
 function App() {
-    return <div>App</div>
+    const [books, setBooks] = useState([]);
+    //event handler to pass as prop
+    //for custom event handler we can pass prop name of our choice
+    const createBook = (title) => {
+        console.log('Need to add book with', title)
+    };
+    return <div>
+        <BookCreate onCreate={createBook} />
+    </div>
 }
 
 export default App;
--- 4_book/src/components/BookCreate.js
+++ 4_book/src/components/BookCreate.js
@@ -1,5 +1,28 @@
-function BookCreate() {
-    return <div>BookCreate</div>
+import { useState } from "react";
+
+function BookCreate({ onCreate }) {
+    const [title, setTitle] = useState('');
+
+    //watch for change on input form
+    const handleChange = (event) => {
+        setTitle(event.target.value);
+    };
+
+    const handleSubmit = (event) => {
+        event.preventDefault();
+        onCreate(title);
+        //when form gets submitted form input feild we want impur field to be empty
+        //This is acheive by making setter function empty
+        setTitle('')
+    }
+    return <div>
+        <form onSubmit={handleSubmit}>
+            <label>Title</label>
+            {/* wiring up title and handle change in input element */}
+            <input value={title} onChange={handleChange} />
+            <button>Create!</button>
+        </form>
+    </div>
 }
 
 export default BookCreate;
