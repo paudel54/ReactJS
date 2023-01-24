4cf15997cf916377a046f25db2040d19e9a1fcd9
paudel54 <2sansrit@gmail.com>
2023-01-24 09:16:42 +0545

    toggling Form Display

--- 4_book/src/components/BookShow.js
+++ 4_book/src/components/BookShow.js
@@ -1,15 +1,33 @@
+import BookEdit from './BookEdit';
+import { useState } from "react";
+
 function BookShow({ book, onDelete }) {
-    console.log(onDelete);
-    const handleClick = () => {
+    const [showEdit, setShowEdit] = useState(false);
+
+    const handleDeleteClick = () => {
         onDelete(book.id);
     };
 
+    const handleEditClick = () => {
+        setShowEdit(!showEdit);
+    }
+
+    let content = <h3>{book.title}</h3>;
+    if (showEdit) {
+        content = <BookEdit />;
+    }
+
 
     return <div className="book-show">
-        {book.title}
+        {/* {book.title} */}
+        {/* to make toggle functional we set content here */}
+        <div>{content}</div>
         <div className="actions">
-            <button className="delete" onClick={handleClick}>
-                del
+            <button className="edit" onClick={handleEditClick}>
+                Edit
+            </button>
+            <button className="delete" onClick={handleDeleteClick}>
+                delete
             </button>
         </div>
     </div>
