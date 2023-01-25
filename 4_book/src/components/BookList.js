549f41cdcbad8959ad947fc13efa58208b4740f7
paudel54 <2sansrit@gmail.com>
2023-01-25 18:36:49 +0545

    project Refactoring initiated

--- 4_book/src/components/BookList.js
+++ 4_book/src/components/BookList.js
@@ -3,13 +3,14 @@ import BooksContext from '../context/books';
 import { useContext } from 'react';
 
 function BookList({ books, onDelete, onEdit }) {
-    const value = useContext(BooksContext);
+
+
     const renderedBooks = books.map((book) => {
         return <BookShow key={book.id} book={book} onDelete={onDelete} onEdit={onEdit} />
     })
 
     return <div className='book-list'>
-        {value}
+
         {renderedBooks}
     </div>
 }
--- 4_book/src/context/books.js
+++ 4_book/src/context/books.js
@@ -1,6 +1,20 @@
 // created Books Context 
-import { createContext } from 'react';
+import { createContext, useState } from 'react';
 
 const BooksContext = createContext();
 
+//new component Provider
+function Provider({ children }) {
+
+
+    return <BooksContext.Provider value={{}}>
+        {children}
+    </BooksContext.Provider>
+}
+// custom export
+export { Provider };
 export default BooksContext;
+
+//to use both exports in other files we can write it as :
+
+//import BooksContext, { Provider} from './'
