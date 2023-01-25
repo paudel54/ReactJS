6faaed619f48129c857f97c573d6ec482be00c42
paudel54 <2sansrit@gmail.com>
2023-01-25 13:26:00 +0545

    consuming context, communication with context without props

--- 4_book/src/components/BookList.js
+++ 4_book/src/components/BookList.js
@@ -1,11 +1,17 @@
 import BookShow from './BookShow';
+import BooksContext from '../context/books';
+import { useContext } from 'react';
 
 function BookList({ books, onDelete, onEdit }) {
+    const value = useContext(BooksContext);
     const renderedBooks = books.map((book) => {
         return <BookShow key={book.id} book={book} onDelete={onDelete} onEdit={onEdit} />
     })
 
-    return <div className='book-list'>{renderedBooks} </div>
+    return <div className='book-list'>
+        {value}
+        {renderedBooks}
+    </div>
 }
 
 export default BookList; 
