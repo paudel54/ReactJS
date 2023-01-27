23d04291ba0368bc7b1cdf0ad9a3323f1bfeb69e
paudel54 <2sansrit@gmail.com>
2023-01-27 16:27:42 +0545

    prototype in actions

--- 5_comps/src/App.js
+++ 5_comps/src/App.js
@@ -1,5 +1,33 @@
+import Button from './Button';
+
 function App() {
-    return <div>Hello Mr Kim</div>
+    return (
+        <div>
+            <div>
+                <Button success rounded outline >
+                    Click me!!
+                </Button>
+            </div>
+            <div>
+                <Button danger outline>
+                    Buy Now!
+                </Button>
+            </div>
+            <div>
+                <Button warning>See Deal!</Button>
+            </div>
+            <div>
+                <Button secondary outline>
+                    Hide Ads!
+                </Button>
+            </div>
+            <div>
+                <Button secondary rounded>
+                    Something!
+                </Button>
+            </div>
+        </div>
+    );
 }
 
 export default App;
---
+++ 5_comps/src/Button.js
@@ -0,0 +1,41 @@
+import PropTypes from 'prop-types';
+
+// here children in arg auto receives the probs of Button , say the text inside button passed from the App.js comp files. 
+// here function props will be passed down and validated its type by fn.PropTypes 
+function Button(
+    {
+        children,
+        primary,
+        secondary,
+        success,
+        warning,
+        danger,
+        outline,
+        rounded,
+    }) {
+    return <button>{children}</button>
+}
+
+//adding custom key to this prop object
+// prop type js lib, old days, pass prop of boolean, helper library , still popular but typescript ruled today
+// custom validation feature, we mark only one of the primary,sec,success,waring,danger is true
+Button.propTypes = {
+    // porps receives props passed on from app.js
+    checkVariationValue: ({ primary, secondary, success, warning, danger }) => {
+        // console.log(props);
+        // validation of Number
+        const count = Number(!!primary)
+            + Number(!!secondary)
+            + Number(!!warning)
+            + Number(!!success)
+            + Number(!!danger)
+
+        if (count > 1) {
+            return new Error('Only one of primary, secondary, success, warning, danger can be true');
+        }
+    },
+};
+
+
+
+export default Button;
