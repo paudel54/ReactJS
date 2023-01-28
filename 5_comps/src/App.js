31bed12cf78fd3248e401500b9e5a2f305047875
paudel54 <2sansrit@gmail.com>
2023-01-28 10:01:13 +0545

    building list preview

--- 5_comps/src/App.js
+++ 5_comps/src/App.js
@@ -4,15 +4,19 @@ function App() {
     const items = [
         // contains array of objects
         {
+            id: 'randl34',
             label: 'Can I use React on a Project?',
             content: 'You can use React on anyproject you want'
 
         },
         {
+
+            id: 'oun34',
             label: 'Can I use JavaScript on a Project?',
             content: 'You can use JavaScript on anyproject you want'
         },
         {
+            id: '98uy',
             label: 'Can I use CSS on a Project?',
             content: 'You can use CSS on anyproject you want'
         }
--- 5_comps/src/components/Accordion.js
+++ 5_comps/src/components/Accordion.js
@@ -1,7 +1,18 @@
 // created component named Accordion.
 function Accordion({ items }) {
+    const renderedItems = items.map((item) => {
+        return (
+            // on building list the parent div must possess id, that may in future come from external api or anythings,
 
-    return <div />
+            <div key={item.id}>
+                <div>{item.label}</div>
+                <div>{item.content}</div>
+            </div>
+        );
+    });
+    return <div>
+        {renderedItems}
+    </div>;
 }
 
 export default Accordion;
