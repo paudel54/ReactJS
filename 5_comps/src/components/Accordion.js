8265a4b6a71a228e03cd6025ae1e755335ddd95e
paudel54 <2sansrit@gmail.com>
2023-01-29 08:50:32 +0545

    Clearified map fn in JS and react

--- 5_comps/src/components/Accordion.js
+++ 5_comps/src/components/Accordion.js
@@ -5,18 +5,33 @@ import { useState } from 'react';
 function Accordion({ items }) {
     // state var and setter fn for expandedIndex, with  a default value 0. i,e first obj ati 0 index will be 
     //expanded other would be collasped. 
+    // console.log(items);
     const [expandedIndex, setExpandedIndex] = useState(0);
 
-    const renderedItems = items.map((item) => {
+    // type 1 to map and access
+    // const renderedItems = items.map((item, index) => {
+    // if (index === expandedIndex) {
+    //     console.log('expanded');
+    // } else {
+    //     console.log('collapsed');
+    // }
+    const renderedItems = items.map((item, index) => {
+        const isExpanded = index === expandedIndex;
+        console.log(isExpanded);
+
+
         return (
             // on building list the parent div must possess id, that may in future come from external api or anythings,
 
             <div key={item.id}>
+
                 <div>{item.label}</div>
                 <div>{item.content}</div>
             </div>
         );
+
     });
+
     return <div>
         {renderedItems}
     </div>;
