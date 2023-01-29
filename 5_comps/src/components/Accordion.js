0c715844d65e788f5ce2c8db989b8bb8a27a0afe
paudel54 <2sansrit@gmail.com>
2023-01-29 08:30:31 +0545

    More focus on state system, initiated expanded state

--- 5_comps/src/components/Accordion.js
+++ 5_comps/src/components/Accordion.js
@@ -1,5 +1,12 @@
+// more learning of react state system
+import { useState } from 'react';
+
 // created component named Accordion.
 function Accordion({ items }) {
+    // state var and setter fn for expandedIndex, with  a default value 0. i,e first obj ati 0 index will be 
+    //expanded other would be collasped. 
+    const [expandedIndex, setExpandedIndex] = useState(0);
+
     const renderedItems = items.map((item) => {
         return (
             // on building list the parent div must possess id, that may in future come from external api or anythings,
