f6c93af4e25deccd6868daabe7b4f76e9f06ded2
paudel54 <2sansrit@gmail.com>
2023-02-01 07:13:36 +0545

    Implemented Dropdown State and handlers

--- 5_comps/src/components/Dropdown.js
+++ 5_comps/src/components/Dropdown.js
@@ -1,4 +1,36 @@
+import { useState } from 'react';
+
 function Dropdown({ options }) {
-    return <div>Dropdown Text</div>
+    const [isOpen, setIsOpen] = useState(false);
+    // event handler
+    const handleClick = () => {
+        // if false true if true false, !isOpen => notisOpen
+        // functional appraoch: setIsOpen((currentIsOpen)=> !currentIsOpen);
+        setIsOpen(!isOpen);
+    };
+    // Here option is an arugument variable
+    const handleOptionClick = (option) => {
+        // console.log('I should Close');
+        // close dropdown
+        //  what option did the use clicked?
+        setIsOpen(!isOpen);
+        console.log(option);
+    }
+
+    const renderedOptions = options.map((option) => {
+        return <div onClick={() => handleOptionClick(option)} key={option.value}>
+            {/* {console.log(option)} */}
+            {/* Object { label: "Red", value: "red" }
+            Object { label: "Green", value: "green" }
+            Object { label: "Blue", value: "blue" } */}
+            {option.label}
+        </div>
+    });
+
+    return <div onClick={handleClick}>
+        Select....
+        {/* adding logic to hide and show txt */}
+        {isOpen && <div> {renderedOptions}</div>}
+    </div>
 }
-export default Dropdown;
+export default Dropdown; 
