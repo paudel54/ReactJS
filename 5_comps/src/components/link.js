9a3424c1e5d6fe9c1028fd75c66812ebc23df5f5
paudel54 <2sansrit@gmail.com>
2023-02-02 17:11:07 +0545

    added style on the button with tailwind classes

--- 5_comps/src/components/link.js
+++ 5_comps/src/components/link.js
@@ -1,12 +1,14 @@
 // consider this receives 2 major props to, to receive link address and children to receive text on anchor element
 import { useContext } from "react";
 import NavigationContext from "../context/navigation";
+import classNames from "classnames";
 
 function Link({ to, children }) {
     // useContext(NavigationContext) returns objects, we only care navigation fn
     const { navigate } = useContext(NavigationContext)
     // console.log(useContext(NavigationContext));
 
+    const classes = classNames('text-blue-500')
 
     const handleClick = (event) => {
         // implemented on click, open new tab and navigate functionality!
@@ -16,7 +18,9 @@ function Link({ to, children }) {
         event.preventDefault();
         navigate(to);
     };
-    return <a onClick={handleClick} href={to}>{children}</a>
+    return <a className={classes} onClick={handleClick} href={to}>
+        {children}
+    </a>
 }
 
 export default Link;
