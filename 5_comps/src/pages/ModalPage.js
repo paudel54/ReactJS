56ac56c11dde8bcdd7edd4aef31044e2949ee3c4
paudel54 <2sansrit@gmail.com>
2023-02-04 17:46:20 +0545

    Bug occured , on model opened the bg text comes on scroll, we need to disable scroll to fix a bug

--- 5_comps/src/pages/ModalPage.js
+++ 5_comps/src/pages/ModalPage.js
@@ -30,6 +30,48 @@ function ModalPage() {
             </Button>
             {/* shows first false value:blank and last truty value when clicked */}
             {showModal && modal}
+            <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Quae voluptatum neque tempora molestias inventore nostrum officia voluptatibus a nisi fugit impedit, exercitationem rerum saepe hic repellat assumenda magni, unde nihil.
+                Lorem ipsum dolor sit amet consectetur, adipisicing elit. Corrupti adipisci velit tempora ducimus quas blanditiis, explicabo fugit omnis optio iste corporis ad ipsum iure tenetur architecto similique, voluptatibus cum excepturi.
+                Lorem ipsum dolor sit, amet consectetur adipisicing elit. Adipisci aliquam tempore consequuntur architecto sunt culpa ut qui, aperiam earum illo. Iusto dolores quaerat recusandae ipsum officia exercitationem suscipit non voluptate?
+                Lorem ipsum dolor sit amet consectetur adipisicing elit. Expedita, tempora tenetur? Perferendis facilis voluptatum libero quam, velit impedit ex quae dolorem temporibus animi delectus assumenda! Veritatis qui ab magnam sunt.
+                Lorem ipsum dolor sit amet consectetur adipisicing elit. Voluptates odio, porro sunt voluptas quam explicabo a adipisci placeat saepe repellat, dolorem sapiente temporibus quisquam, suscipit quas fugiat aliquid deleniti hic?
+                Lorem ipsum dolor sit amet, consectetur adipisicing elit. Est praesentium animi voluptate, ex, mollitia tenetur cumque placeat itaque incidunt, sint a modi sequi quo! Voluptates sapiente nulla quae voluptatem mollitia.
+            </p>
+            <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Quae voluptatum neque tempora molestias inventore nostrum officia voluptatibus a nisi fugit impedit, exercitationem rerum saepe hic repellat assumenda magni, unde nihil.
+                Lorem ipsum dolor sit amet consectetur, adipisicing elit. Corrupti adipisci velit tempora ducimus quas blanditiis, explicabo fugit omnis optio iste corporis ad ipsum iure tenetur architecto similique, voluptatibus cum excepturi.
+                Lorem ipsum dolor sit, amet consectetur adipisicing elit. Adipisci aliquam tempore consequuntur architecto sunt culpa ut qui, aperiam earum illo. Iusto dolores quaerat recusandae ipsum officia exercitationem suscipit non voluptate?
+                Lorem ipsum dolor sit amet consectetur adipisicing elit. Expedita, tempora tenetur? Perferendis facilis voluptatum libero quam, velit impedit ex quae dolorem temporibus animi delectus assumenda! Veritatis qui ab magnam sunt.
+                Lorem ipsum dolor sit amet consectetur adipisicing elit. Voluptates odio, porro sunt voluptas quam explicabo a adipisci placeat saepe repellat, dolorem sapiente temporibus quisquam, suscipit quas fugiat aliquid deleniti hic?
+                Lorem ipsum dolor sit amet, consectetur adipisicing elit. Est praesentium animi voluptate, ex, mollitia tenetur cumque placeat itaque incidunt, sint a modi sequi quo! Voluptates sapiente nulla quae voluptatem mollitia.
+            </p>
+            <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Quae voluptatum neque tempora molestias inventore nostrum officia voluptatibus a nisi fugit impedit, exercitationem rerum saepe hic repellat assumenda magni, unde nihil.
+                Lorem ipsum dolor sit amet consectetur, adipisicing elit. Corrupti adipisci velit tempora ducimus quas blanditiis, explicabo fugit omnis optio iste corporis ad ipsum iure tenetur architecto similique, voluptatibus cum excepturi.
+                Lorem ipsum dolor sit, amet consectetur adipisicing elit. Adipisci aliquam tempore consequuntur architecto sunt culpa ut qui, aperiam earum illo. Iusto dolores quaerat recusandae ipsum officia exercitationem suscipit non voluptate?
+                Lorem ipsum dolor sit amet consectetur adipisicing elit. Expedita, tempora tenetur? Perferendis facilis voluptatum libero quam, velit impedit ex quae dolorem temporibus animi delectus assumenda! Veritatis qui ab magnam sunt.
+                Lorem ipsum dolor sit amet consectetur adipisicing elit. Voluptates odio, porro sunt voluptas quam explicabo a adipisci placeat saepe repellat, dolorem sapiente temporibus quisquam, suscipit quas fugiat aliquid deleniti hic?
+                Lorem ipsum dolor sit amet, consectetur adipisicing elit. Est praesentium animi voluptate, ex, mollitia tenetur cumque placeat itaque incidunt, sint a modi sequi quo! Voluptates sapiente nulla quae voluptatem mollitia.
+            </p>
+            <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Quae voluptatum neque tempora molestias inventore nostrum officia voluptatibus a nisi fugit impedit, exercitationem rerum saepe hic repellat assumenda magni, unde nihil.
+                Lorem ipsum dolor sit amet consectetur, adipisicing elit. Corrupti adipisci velit tempora ducimus quas blanditiis, explicabo fugit omnis optio iste corporis ad ipsum iure tenetur architecto similique, voluptatibus cum excepturi.
+                Lorem ipsum dolor sit, amet consectetur adipisicing elit. Adipisci aliquam tempore consequuntur architecto sunt culpa ut qui, aperiam earum illo. Iusto dolores quaerat recusandae ipsum officia exercitationem suscipit non voluptate?
+                Lorem ipsum dolor sit amet consectetur adipisicing elit. Expedita, tempora tenetur? Perferendis facilis voluptatum libero quam, velit impedit ex quae dolorem temporibus animi delectus assumenda! Veritatis qui ab magnam sunt.
+                Lorem ipsum dolor sit amet consectetur adipisicing elit. Voluptates odio, porro sunt voluptas quam explicabo a adipisci placeat saepe repellat, dolorem sapiente temporibus quisquam, suscipit quas fugiat aliquid deleniti hic?
+                Lorem ipsum dolor sit amet, consectetur adipisicing elit. Est praesentium animi voluptate, ex, mollitia tenetur cumque placeat itaque incidunt, sint a modi sequi quo! Voluptates sapiente nulla quae voluptatem mollitia.
+            </p>
+            <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Quae voluptatum neque tempora molestias inventore nostrum officia voluptatibus a nisi fugit impedit, exercitationem rerum saepe hic repellat assumenda magni, unde nihil.
+                Lorem ipsum dolor sit amet consectetur, adipisicing elit. Corrupti adipisci velit tempora ducimus quas blanditiis, explicabo fugit omnis optio iste corporis ad ipsum iure tenetur architecto similique, voluptatibus cum excepturi.
+                Lorem ipsum dolor sit, amet consectetur adipisicing elit. Adipisci aliquam tempore consequuntur architecto sunt culpa ut qui, aperiam earum illo. Iusto dolores quaerat recusandae ipsum officia exercitationem suscipit non voluptate?
+                Lorem ipsum dolor sit amet consectetur adipisicing elit. Expedita, tempora tenetur? Perferendis facilis voluptatum libero quam, velit impedit ex quae dolorem temporibus animi delectus assumenda! Veritatis qui ab magnam sunt.
+                Lorem ipsum dolor sit amet consectetur adipisicing elit. Voluptates odio, porro sunt voluptas quam explicabo a adipisci placeat saepe repellat, dolorem sapiente temporibus quisquam, suscipit quas fugiat aliquid deleniti hic?
+                Lorem ipsum dolor sit amet, consectetur adipisicing elit. Est praesentium animi voluptate, ex, mollitia tenetur cumque placeat itaque incidunt, sint a modi sequi quo! Voluptates sapiente nulla quae voluptatem mollitia.
+            </p>
+            <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Quae voluptatum neque tempora molestias inventore nostrum officia voluptatibus a nisi fugit impedit, exercitationem rerum saepe hic repellat assumenda magni, unde nihil.
+                Lorem ipsum dolor sit amet consectetur, adipisicing elit. Corrupti adipisci velit tempora ducimus quas blanditiis, explicabo fugit omnis optio iste corporis ad ipsum iure tenetur architecto similique, voluptatibus cum excepturi.
+                Lorem ipsum dolor sit, amet consectetur adipisicing elit. Adipisci aliquam tempore consequuntur architecto sunt culpa ut qui, aperiam earum illo. Iusto dolores quaerat recusandae ipsum officia exercitationem suscipit non voluptate?
+                Lorem ipsum dolor sit amet consectetur adipisicing elit. Expedita, tempora tenetur? Perferendis facilis voluptatum libero quam, velit impedit ex quae dolorem temporibus animi delectus assumenda! Veritatis qui ab magnam sunt.
+                Lorem ipsum dolor sit amet consectetur adipisicing elit. Voluptates odio, porro sunt voluptas quam explicabo a adipisci placeat saepe repellat, dolorem sapiente temporibus quisquam, suscipit quas fugiat aliquid deleniti hic?
+                Lorem ipsum dolor sit amet, consectetur adipisicing elit. Est praesentium animi voluptate, ex, mollitia tenetur cumque placeat itaque incidunt, sint a modi sequi quo! Voluptates sapiente nulla quae voluptatem mollitia.
+            </p>
         </div>
     );
 }
