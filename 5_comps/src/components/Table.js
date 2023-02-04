c70c1610e87fa719f9fac4413ed5e0d13aa9cf1a
paudel54 <2sansrit@gmail.com>
2023-02-04 21:39:56 +0545

    Building Table List With Mapping FUnction

--- 5_comps/src/components/Table.js
+++ 5_comps/src/components/Table.js
@@ -1,5 +1,26 @@
 function Table({ data }) {
-    return <div>{data.length}</div>
+    const renderedRows = data.map((fruit) => {
+        return (
+            <tr key={fruit.name}>
+                <td>{fruit.name}</td>
+                <td>{fruit.color}</td>
+                <td>{fruit.score}</td>
+            </tr>
+        );
+    });
+    return <table>
+        <thead>
+            <tr>
+                <th>Fruit</th>
+                <th>Color</th>
+                <th>Score</th>
+            </tr>
+        </thead>
+
+        <tbody>
+            {renderedRows}
+        </tbody>
+    </table>
 }
 
 export default Table;
