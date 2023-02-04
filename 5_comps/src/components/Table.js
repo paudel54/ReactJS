76455fa2e3d14e140b1b519b49daba8597551357
paudel54 <2sansrit@gmail.com>
2023-02-04 21:56:03 +0545

    Created Table but not reuseable

--- 5_comps/src/components/Table.js
+++ 5_comps/src/components/Table.js
@@ -1,16 +1,16 @@
 function Table({ data }) {
     const renderedRows = data.map((fruit) => {
         return (
-            <tr key={fruit.name}>
-                <td>{fruit.name}</td>
-                <td>{fruit.color}</td>
-                <td>{fruit.score}</td>
+            <tr className="border-b" key={fruit.name}>
+                <td className="p-3">{fruit.name}</td>
+                <td className="p-3"> <div className={`p-3 m-2 ${fruit.color}`}></div></td>
+                <td className="p-3">{fruit.score}</td>
             </tr>
         );
     });
-    return <table>
+    return <table className="table-auto border-spacing-2">
         <thead>
-            <tr>
+            <tr className="border-b-2">
                 <th>Fruit</th>
                 <th>Color</th>
                 <th>Score</th>
