4569c93876481139b22239f274b3e25b073c51ec
paudel54 <2sansrit@gmail.com>
2023-02-04 16:00:32 +0545

    Created Modals Simple Framawork will change later

--- 5_comps/src/components/Modal.js
+++ 5_comps/src/components/Modal.js
@@ -1,5 +1,12 @@
 function Modal() {
-    return <div>Modal!</div>
+    return <div>
+        <div className="absolute inset-0 bg-gray-300 opacity-80">
+
+        </div>
+        <div className="absolute inset-40 p-10 bg-white">
+            I 'iam' a modal!!
+        </div>
+    </div>
 }
 
 export default Modal;
--- 5_comps/src/pages/ModalPage.js
+++ 5_comps/src/pages/ModalPage.js
@@ -1,9 +1,20 @@
+import { useState } from 'react';
 import Modal from '../components/Modal';
-
+import Button from '../components/Button';
 function ModalPage() {
+    const [showModal, setShowModal] = useState(false);
+
+    const handleClick = () => {
+        setShowModal(true);
+    };
+
     return (
         <div>
-            <Modal />
+            <Button onClick={handleClick} primary>
+                Open Modal
+            </Button>
+            {/* shows first false value:blank and last truty value when clicked */}
+            {showModal && <Modal />}
         </div>
     );
 }
