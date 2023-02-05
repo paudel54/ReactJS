3f070a0f266c9c037b40dda784eebc7639bdf678
paudel54 <2sansrit@gmail.com>
2023-02-05 15:32:12 +0545

    implemented react fragment component need console error fix

--- 5_comps/src/components/Table.js
+++ 5_comps/src/components/Table.js
@@ -1,15 +1,17 @@
+import { Fragment } from 'react';
+
 function Table({ data, config, keyFn }) {
 
     const renderedHeaders = config.map((column) => {
         if (column.header) {
-            return column.header();
+            return <Fragment key={column.label}>{column.header()}</Fragment>;
         }
         return <th key={column.label}>{column.label}</th>
     });
 
     const renderedRows = data.map((rowData) => {
         const renderedCells = config.map((column) => {
-            return <td className="p-2" key={column.label}>{column.render(rowData)}</td>
+            return <td classNam e="p-2" key={column.label}>{column.render(rowData)}</td>
         });
         return (
             <tr className="border-b" key={keyFn(rowData)}>
