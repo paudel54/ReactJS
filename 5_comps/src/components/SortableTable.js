38ef41ab71eee559bf5a8747e4fa7ebbf5facc08
paudel54 <2sansrit@gmail.com>
2023-02-05 20:07:53 +0545

    implemented sort worked, need code review now

--- 5_comps/src/components/SortableTable.js
+++ 5_comps/src/components/SortableTable.js
@@ -8,7 +8,7 @@ function SortableTable(props) {
     // TO keep track of which column we are sorting by
     const [sortBy, setSortBy] = useState(null);
 
-    const { config } = props;
+    const { config, data } = props;
 
     const handleClick = (label) => {
         // console.log(label);
@@ -35,10 +35,28 @@ function SortableTable(props) {
         };
 
     });
+    // Only sort data if sortOrder && sortBy are not null. unsorted
+    // MAKE a copy of the 'data prop
+    // FInd the correct sortValue function and use it for sorting. 
+    let sortedData = data;
+    if (sortOrder && sortBy) {
+        const { sortValue } = config.find(column => column.label === sortBy);
+        sortedData = [...data].sort((a, b) => {
+            const valueA = sortValue(a);
+            const valueB = sortValue(b);
 
+            const reverseOrder = sortOrder === 'asc' ? 1 : -1;
+
+            if (typeof valueA === 'string') {
+                return valueA.localeCompare(valueB) * reverseOrder;
+            } else {
+                return (valueA - valueB) * reverseOrder;
+            }
+        });
+    }
     return <div>
         {sortOrder} - {sortBy}
-        <Table {...props} config={updatedConfig} />;
+        <Table {...props} data={sortedData} config={updatedConfig} />;
     </div>
 
 }
