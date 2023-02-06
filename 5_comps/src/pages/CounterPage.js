32694314f1e75be2f81f2572a868a2cf3ac71ba0
paudel54 <2sansrit@gmail.com>
2023-02-06 12:56:17 +0545

    Application mini complete with useState Hook

--- 5_comps/src/pages/CounterPage.js
+++ 5_comps/src/pages/CounterPage.js
@@ -6,6 +6,7 @@ import Panel from '../components/Panel';
 // considering this component receives intial props and set defalut value for count
 function CounterPage({ intialCount }) {
     const [count, setCount] = useState(intialCount);
+    const [valueToAdd, setValueToAdd] = useState(0);
 
     const increment = () => {
         setCount(count + 1);
@@ -14,6 +15,19 @@ function CounterPage({ intialCount }) {
     const decrement = () => {
         setCount(count - 1);
     };
+
+    // whenever event hander is called it gets called with the event object
+    const handleChange = (event) => {
+        const value = parseInt(event.target.value) || 0;
+        setValueToAdd(value);
+    };
+    // Whenever we submit a form brower by default tries to submit form for us we want to prevent that default behaviour.
+
+    const handleSubmit = (event) => {
+        event.preventDefault();
+        setCount(count + valueToAdd);
+        setValueToAdd(0);
+    }
 
     return (
         <Panel className="m-3">
@@ -24,9 +38,14 @@ function CounterPage({ intialCount }) {
                 <Button onClick={decrement}> Decrement </Button>
             </div>
 
-            <form>
+            <form onSubmit={handleSubmit}>
                 <label>Add a lot!</label>
-                <input type="number" className='p-1 m-3 bg-gray-50 border border-gray-300' />
+                {/* onto input feild the onChange handler passes and event prop to handler defined */}
+                <input
+                    value={valueToAdd || ""}
+                    onChange={handleChange}
+                    type="number"
+                    className='p-1 m-3 bg-gray-50 border border-gray-300' />
                 <Button>Add it!</Button>
             </form>
         </Panel>
