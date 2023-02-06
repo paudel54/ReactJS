9142bedac8fcbbabd2b41af78146070468bb4939
paudel54 <2sansrit@gmail.com>
2023-02-06 15:49:09 +0545

    understanding action object useReducer

--- 5_comps/src/pages/CounterPage.js
+++ 5_comps/src/pages/CounterPage.js
@@ -4,7 +4,33 @@ import Button from '../components/Button';
 import Panel from '../components/Panel';
 
 const reducer = (state, action) => {
-    // 
+    // return {
+    //     // copies all the prop keys and val from state obj
+    //     ...state,
+    //     count: state.count + 1
+
+    // }
+    console.log(state);
+
+    if (action.type === 'increment') {
+        // return is required here, if no returned for this the state would be automatically defined
+        // to undefined.
+        return {
+            ...state,
+            count: state.count + 1
+        };
+    }
+
+    if (action.type === 'change-value-to-add') {
+        return {
+            ...state,
+            valueToAdd: action.payload
+        };
+
+    }
+
+    // even though no valid condition are met we can have return state
+    return state;
 };
 
 // considering this component receives intial props and set defalut value for count
@@ -20,6 +46,10 @@ function CounterPage({ intialCount }) {
     const increment = () => {
         // useReducer: useful when future state vlaue depends on current State
         // setCount(count + 1);
+        // dispatch an action object that must have a type property
+        dispatch({
+            type: 'increment'
+        });
     };
 
     const decrement = () => {
@@ -30,6 +60,11 @@ function CounterPage({ intialCount }) {
     const handleChange = (event) => {
         const value = parseInt(event.target.value) || 0;
         // setValueToAdd(value);
+        // dispatch an action object, which must have type of string, 
+        dispatch({
+            type: 'change-value-to-add',
+            payload: value
+        });
     };
     // Whenever we submit a form brower by default tries to submit form for us we want to prevent that default behaviour.
 
