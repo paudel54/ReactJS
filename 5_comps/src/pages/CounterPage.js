cc9ce4652df6025b757cd933cebb89720bdbba0a
paudel54 <2sansrit@gmail.com>
2023-02-06 12:14:13 +0545

    erased custom hook to implement counter logic with new hook useReducer

--- 5_comps/src/pages/CounterPage.js
+++ 5_comps/src/pages/CounterPage.js
@@ -1,11 +1,9 @@
 import Button from '../components/Button';
-import useCounter from '../hooks/use-counter';
+
 
 // considering this component receives intial props and set defalut value for count
 function CounterPage({ intialCount }) {
-    // Calling the function and destructuring the component needs.
-    // passing prop as an argument to 
-    const { count, increment } = useCounter(intialCount);
+
     return <div>
         <h1> Count is {count} </h1>
         <Button onClick={increment}>
--- jsx/src/App.js
+++ jsx/src/App.js
@@ -3,4 +3,5 @@ function App() {
     return <h1>Hi There!</h1>
 }
 
+//exporting the component so it can be imported from othe files
 export default App;
