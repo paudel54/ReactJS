4bac967cf6d60ea45e03eaa4407c413b81600e95
paudel54 <2sansrit@gmail.com>
2023-02-10 14:02:54 +0545

    added total cost functionality

--- 6_cars/src/components/CarValue.js
+++ 6_cars/src/components/CarValue.js
@@ -1,5 +1,28 @@
+// need acess to show total cost 
+import { useSelector } from "react-redux";
+
+
 function CarValue() {
-    return <div>Carvalue</div>
+    // look onto cars peice of state and pick req method
+    const totalCost = useSelector(({ cars: { data, searchTerm } }) => {
+
+        const filteredCars = data.filter((car) => {
+            return car.name.toLowerCase().includes(searchTerm.toLowerCase());
+        });
+
+        //   for (variable of iterable)
+        //   statement
+        let cost = 0;
+        for (let car of filteredCars) {
+            cost += car.cost
+        }
+        return cost;
+
+    });
+
+    return <div className="car-value">
+        Total Cost: ${totalCost}
+    </div>
 }
 
-export default CarValue;
+export default CarValue; 
