4d9c12456eac3e921edfe2338d6d2e3380bea77b
paudel54 <2sansrit@gmail.com>
2023-02-15 09:26:54 +0545

    rendering list with mapping element

--- 7_media/src/components/AlbumsList.js
+++ 7_media/src/components/AlbumsList.js
@@ -1,10 +1,35 @@
 import { useFetchAlbumsQuery } from "../store";
+import Skeleton from './Skeleton';
+import ExpandablePanel from './ExpandablePanel';
+import Button from './Button';
 
 function AlbumsList({ user }) {
+    // const results = useFetchAlbumsQuery(user);
     const { data, error, isLoading } = useFetchAlbumsQuery(user);
-    console.log(data, error, isLoading);
+    // console.log(data, error, isLoading);
+    // console.log(results);
+
+    let content;
+    if (isLoading) {
+        content = <Skeleton times={3} />
+    } else if (error) {
+        content = <div> Error Loading Albums.</div>
+    } else {
+        content = data.map(album => {
+            const header = <div>{album.title}</div>;
+            return <ExpandablePanel key={album.id} header={header}>
+                List of Photos in the album
+            </ExpandablePanel>
+        });
+    }
+
     return <div>
-        Albums For {user.name}
+        <div>
+            Albums For {user.name}
+        </div>
+        <div>
+            {content}
+        </div>
     </div>
 }
 export default AlbumsList;
