c0fc54f8e6bda58ff54ceb93cbf18a221e19aaed
paudel54 <2sansrit@gmail.com>
2023-02-15 16:48:17 +0545

    Created photos  listc

--- 7_media/src/components/PhotosList.js
+++ 7_media/src/components/PhotosList.js
@@ -1,10 +1,25 @@
-import { useFetchPhotosQuery } from "../store";
+import { useFetchPhotosQuery, useAddPhotoMutation } from "../store";
+import Button from './Button';
+
 
 
 function PhotosList({ album }) {
     // on mutation requires album
     useFetchPhotosQuery(album);
-    return 'PhotosListss'
+
+    const handleAddPhoto = () => {
+        addPhoto(album);
+    };
+
+    const [addPhoto, addPhotoResults] = useAddPhotoMutation();
+    return <div>
+        <div className="m-2 flex flex-row items-center justify-between">
+            <h3 className="text-lg font-bold">Photos In {album.title} </h3>
+            <Button loading={addPhotoResults.isLoading} onClick={handleAddPhoto}>
+                + Add Photo
+            </Button>
+        </div>
+    </div>
 }
 
 export default PhotosList;
