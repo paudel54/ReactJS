3475997960cf24d0eca2af6b9982ab5d4c7766c9
paudel54 <2sansrit@gmail.com>
2023-02-18 19:25:20 +0545

    done with floating nav with state change

--- 9_portfoilo/src/components/nav/Nav.jsx
+++ 9_portfoilo/src/components/nav/Nav.jsx
@@ -4,23 +4,51 @@ import { AiOutlineUser } from "react-icons/ai";
 import { RiBook3Line } from "react-icons/ri";
 import { MdWebStories } from "react-icons/md";
 import { TbMessage } from "react-icons/tb";
+import { useState } from "react";
 
 export default function Nav() {
+  const [activeNav, setActiveNav] = useState("#");
   return (
     <nav>
-      <a href="#" className="active">
+      <a
+        href="#"
+        onClick={() => setActiveNav("#")}
+        className={activeNav === "#" ? "active" : ""}
+      >
         <AiFillHome />
       </a>
-      <a href="#about">
+      <a
+        href="#about"
+        onClick={() => setActiveNav("#about")}
+        className={activeNav === "#about" ? "active" : ""}
+      >
         <AiOutlineUser />
       </a>
-      <a href="#experience">
+      <a
+        href="#experience"
+        onClick={() => {
+          setActiveNav("#experience");
+        }}
+        className={activeNav === "#experience" ? "active" : ""}
+      >
         <RiBook3Line />
       </a>
-      <a href="#services">
+      <a
+        href="#services"
+        onClick={() => {
+          setActiveNav("#services");
+        }}
+        className={activeNav === "#services" ? "active" : ""}
+      >
         <MdWebStories />
       </a>
-      <a href="#contact">
+      <a
+        href="#contact"
+        onClick={() => {
+          setActiveNav("#contact");
+        }}
+        className={activeNav === "#contact" ? "active" : ""}
+      >
         <TbMessage />
       </a>
     </nav>
