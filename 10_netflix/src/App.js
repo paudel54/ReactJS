426b27d8f459aa415a783cac885ff05eba3cb930
paudel54 <2sansrit@gmail.com>
2023-02-21 18:12:16 +0545

    react router got setuped

--- 10_netflix/src/App.js
+++ 10_netflix/src/App.js
@@ -1,11 +1,15 @@
 import Navbar from "./components/Navbar";
 // contact:  984-3894718
-
+import { Route, Routes } from "react-router-dom";
+import Home from './pages/Home';
 
 function App() {
   return (
     <>
       <Navbar />
+      <Routes>
+        <Route path='/' element={<Home />} />
+      </Routes>
     </>
   );
 }
--- 10_netflix/src/components/Navbar.jsx
+++ 10_netflix/src/components/Navbar.jsx
@@ -1,4 +1,16 @@
 import React from "react";
 export default function Navbar() {
-  return <div className="text-2xl text-pink-600">Hello all</div>;
+  return (
+    <div className="flex items-center justify-between p-4 z-[100] w-full absolute">
+      <h1 className="text-red-600 text-4xl font-bold cursor-pointer">
+        NETFLIX
+      </h1>
+      <div>
+        <button className="text-white pr-4 ">Sign In</button>
+        <button className="bg-red-600 px-6 py-2 rounded cursor-pointer text-white">
+          Sing Up
+        </button>
+      </div>
+    </div>
+  );
 }
