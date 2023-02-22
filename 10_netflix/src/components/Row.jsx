d91c4e874c7bdd43ad5d57658e12e737c2b010bf
paudel54 <2sansrit@gmail.com>
2023-02-22 12:34:34 +0545

    done with movie component

---
+++ 10_netflix/src/components/Row.jsx
@@ -0,0 +1,30 @@
+import axios from "axios";
+import Movie from "./Movie";
+import { useState, useEffect } from "react";
+export default function Row({ title, fetchURL }) {
+  const [movies, setMovies] = useState([]);
+
+  useEffect(() => {
+    axios.get(fetchURL).then((response) => {
+      setMovies(response.data.results);
+    });
+  }, [fetchURL]);
+
+  console.log(movies);
+  return (
+    <>
+      <h2 className="text-white font-bold md:text-xl p-4">{title}</h2>
+      <div className="relative flex items-center">
+        {/* <div className="text-white">hello</div> */}
+        <div
+          id={"slider"}
+          className="w-full h-full overflow-x-scroll whitespace-nowrap scroll-smooth scrollbar-hide relative"
+        >
+          {movies.map((item, id) => (
+            <Movie key={id} item={item} />
+          ))}
+        </div>
+      </div>
+    </>
+  );
+}
--- 10_netflix/src/pages/Home.jsx
+++ 10_netflix/src/pages/Home.jsx
@@ -1,10 +1,17 @@
-// import Navbar from "../components/Navbar";
 import Main from "../components/Main";
-export default function Home() {
+import Row from "../components/Row";
+import requests from "../Request";
+const Home = () => {
   return (
     <>
-      {/* <Navbar /> */}
       <Main />
+      <Row title="UpComing" fetchURL={requests.requestUpcoming} />
+      <Row title="Popular" fetchURL={requests.requestPopular} />
+      <Row title="Trending" fetchURL={requests.requestTrending} />
+      <Row title="Top Rated" fetchURL={requests.requestTopRated} />
+      <Row title="Horror" fetchURL={requests.requestHorror} />
     </>
   );
-}
+};
+
+export default Home;
