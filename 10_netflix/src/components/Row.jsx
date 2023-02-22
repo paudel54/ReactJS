afda9c40473daa39d4fbad18f5983a9c33a892d8
paudel54 <2sansrit@gmail.com>
2023-02-22 13:59:45 +0545

    UI netflix completed

--- 10_netflix/src/components/Row.jsx
+++ 10_netflix/src/components/Row.jsx
@@ -1,7 +1,9 @@
 import axios from "axios";
 import Movie from "./Movie";
+import { MdChevronLeft, MdChevronRight } from "react-icons/md";
+
 import { useState, useEffect } from "react";
-export default function Row({ title, fetchURL }) {
+export default function Row({ title, fetchURL, rowId }) {
   const [movies, setMovies] = useState([]);
 
   useEffect(() => {
@@ -10,20 +12,42 @@ export default function Row({ title, fetchURL }) {
     });
   }, [fetchURL]);
 
+  const slideLeft = () => {
+    var slider = document.getElementById("slider" + rowId);
+    // console.log(slider);
+    slider.scrollLeft = slider.scrollLeft - 500;
+  };
+
+  const slideRight = () => {
+    var slider = document.getElementById("slider" + rowId);
+    slider.scrollLeft = slider.scrollLeft + 500;
+  };
+
   console.log(movies);
   return (
     <>
       <h2 className="text-white font-bold md:text-xl p-4">{title}</h2>
-      <div className="relative flex items-center">
+      {/* on Hover display icon scroll in row add group class in parent and manipulate on icon class */}
+      <div className="relative flex items-center group">
         {/* <div className="text-white">hello</div> */}
+        <MdChevronLeft
+          onClick={slideLeft}
+          className="left-0 bg-white rounded-full absolute opacity-50 hover:opacity-100 cursor-pointer z-10 hidden group-hover:block"
+          size={40}
+        />
         <div
-          id={"slider"}
-          className="w-full h-full overflow-x-scroll whitespace-nowrap scroll-smooth scrollbar-hide relative"
+          id={"slider" + rowId}
+          className="w-full h-full overflow-x-scroll whitespace-nowrap scroll-smooth scrollbar-hide relative "
         >
           {movies.map((item, id) => (
             <Movie key={id} item={item} />
           ))}
         </div>
+        <MdChevronRight
+          onClick={slideRight}
+          className="right-0 bg-white rounded-full absolute opacity-50 hover:opacity-100 cursor-pointer z-10 hidden group-hover:block"
+          size={40}
+        />
       </div>
     </>
   );
--- 10_netflix/src/pages/Home.jsx
+++ 10_netflix/src/pages/Home.jsx
@@ -5,11 +5,11 @@ const Home = () => {
   return (
     <>
       <Main />
-      <Row title="UpComing" fetchURL={requests.requestUpcoming} />
-      <Row title="Popular" fetchURL={requests.requestPopular} />
-      <Row title="Trending" fetchURL={requests.requestTrending} />
-      <Row title="Top Rated" fetchURL={requests.requestTopRated} />
-      <Row title="Horror" fetchURL={requests.requestHorror} />
+      <Row rowId="1" title="UpComing" fetchURL={requests.requestUpcoming} />
+      <Row rowId="2" title="Popular" fetchURL={requests.requestPopular} />
+      <Row rowId="3" title="Trending" fetchURL={requests.requestTrending} />
+      <Row rowId="4" title="Top Rated" fetchURL={requests.requestTopRated} />
+      <Row rowId="5" title="Horror" fetchURL={requests.requestHorror} />
     </>
   );
 };
