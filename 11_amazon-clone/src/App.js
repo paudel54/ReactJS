48c23ce5f63abbc75d44f2fc8e76da7b88f9d087
paudel54 <2sansrit@gmail.com>
2023-02-24 12:36:15 +0545

    established react router replacing the switch with route and multiple component render with react fragments

--- 11_amazon-clone/src/App.js
+++ 11_amazon-clone/src/App.js
@@ -1,15 +1,24 @@
 import './App.css';
 import Header from './Components/header/Header';
 import Home from './Components/home/Home';
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+
+
 function App() {
   return (
-    <div className="App">
-      {/* <h1>Hello Programmers, onto amazon store 🚀</h1> */}
-      <Header />
-      {/* HEADER */}
-      <Home />
-      {/* Home */}
-    </div>
+    <Router>
+
+      <div className="App">
+        <Routes>
+          {/* Render multiple components in React Router */}
+          {/* use react fregment for multiple rendering  */}
+          <Route path='/' element={<><Header /> <Home /></>} />
+          <Route path='/checkout' element={<><Header /> <h1>Hello from checkout comp</h1></>} />
+        </Routes>
+
+      </div>
+    </Router>
+
   );
 }
 
