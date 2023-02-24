2fe75d7fc0802bb23fb7c016387d82fc35ef95fb
paudel54 <2sansrit@gmail.com>
2023-02-24 07:00:31 +0545

    amazon clone init with added firebase config fileS

---
+++ 11_amazon-clone/src/firebase.js
@@ -0,0 +1,8 @@
+const firebaseConfig = {
+    apiKey: "AIzaSyAQ0hGXoe5w7vInDLsujyJjN7-4LGHgtwc",
+    authDomain: "challenge-77073.firebaseapp.com",
+    projectId: "challenge-77073",
+    storageBucket: "challenge-77073.appspot.com",
+    messagingSenderId: "299759554723",
+    appId: "1:299759554723:web:75490f7610e9b3a0fcaf16"
+};
