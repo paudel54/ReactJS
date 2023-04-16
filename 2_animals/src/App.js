b31df4e9f990682b86369d9d1ec81c1f81bb2141
paudel54 <2sansrit@gmail.com>
2023-04-16 07:56:53 +0545

    Project animals wrapped up

--- 2_animals/src/App.js
+++ 2_animals/src/App.js
@@ -1,22 +1,27 @@
+// JSX is simply an instruction for react to render
 //imported useState module to rerender screen on change in state system;
 import { useState } from 'react';
 import AnimalShow from './AnimalShow';
 import './App.css';
 
 function getRandomAnimal() {
+    console.log('finding out animals in array in random')
     const animals = ['bird', 'cat', 'cow', 'dog', 'gator', 'horse'];
     return animals[Math.floor(Math.random() * animals.length)]
 }
 
-// console.log(getRandomAnimal())
+// console.log(geRandomAnimal())
 
 function App() {
 
     //initiated with empty array
     const [animals, setAnimals] = useState([]);
-
+    console.log('animal initial', animals);
+    console.log('animals after event handling or setter fn', setAnimals)
     const handleClick = () => {
         //calling function getRandomAnimal and stores onto the ...animals array
+        //updates array
+
         setAnimals([...animals, getRandomAnimal()]);
     };
 
@@ -26,11 +31,11 @@ function App() {
         return <AnimalShow type={animal} key={index} />
     });
 
-    console.log(renderedAnimals);
+    // console.log(renderedAnimals);
     return (
-        <div className='app'>
-            <button onClick={handleClick}>Add Animal</button>
-            <div className='animal-list'>{renderedAnimals}</div>
+        <div className='flex flex-col items-center justify-center'>
+            <button className='mt-10 text-white bg-blue-700 shadow-xl hover:bg-blue-800 font-medium rounded-lg px-5 py-2.5 mr-2 mb-2 w-[10rem]' type='button' onClick={handleClick}>Add Animals</button>
+            <div> <div className='flex flex-wrap w-3/4 mx-auto'>{renderedAnimals}</div></div>
         </div>
 
     );
@@ -44,3 +49,4 @@ export default App;
 //SETTLER FUNCTION
 //DESTRUCTURING
 //BUILDING LIST
+
--- 2_animals/src/index.js
+++ 2_animals/src/index.js
@@ -2,6 +2,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import App from './App';
+import './index.css'
 
 const el = document.getElementById('root');
 const root = ReactDOM.createRoot(el);
